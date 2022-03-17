b90e5292f314ce73a4942ede43af33aa1a09cc2b
John William <johnwill.v.2017@gmail.com>
2022-03-16 23:58:11 -0300

    add first project

---
+++ teste-ambiente/src/test/java/br/com/william/testeambiente/TesteAmbienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.william.testeambiente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAmbienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
