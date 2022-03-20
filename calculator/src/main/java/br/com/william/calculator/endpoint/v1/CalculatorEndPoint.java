6875df1bd024032be4d6381db02d204dedb87593
John William <johnwill.v.2017@gmail.com>
2022-03-19 21:13:25 -0300

    move endpoint

--- calculator/src/main/java/br/com/william/calculator/endpoint/v1/CalculatorEndPoint.java
+++ calculator/src/main/java/br/com/william/calculator/endpoint/v1/CalculatorEndPoint.java
@@ -4,14 +4,15 @@ import br.com.william.calculator.service.CalculatorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Author", description = "Endpoints for calculator")
-@RestController
+@Tag(name = "calculatorEndPointV1", description = "Endpoints for calculator")
+@RestController(value = "calculatorEndPointV1")
 @RequestMapping("/v1/calculator")
 @RequiredArgsConstructor
 public class CalculatorEndPoint {
