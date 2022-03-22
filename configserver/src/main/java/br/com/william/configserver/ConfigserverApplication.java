bafbb7f8a87561cb88c89ca46b32688350b9255c
John William <johnwill.v.2017@gmail.com>
2022-03-22 11:12:46 -0300

    add ConfigServer application Spring

---
+++ configserver/src/main/java/br/com/william/configserver/ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package br.com.william.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
