40fe0903b9f5085aa39fcc39778c75207929d061
John William <johnwill.v.2017@gmail.com>
2022-03-24 16:21:00 -0300

    add endpoint Cambio

--- cambio-service/src/main/java/br/com/william/cambioservice/endpoint/CambioEndPoint.java
+++ cambio-service/src/main/java/br/com/william/cambioservice/endpoint/CambioEndPoint.java
@@ -1,39 +1,51 @@
 package br.com.william.cambioservice.endpoint;
 
 import br.com.william.cambioservice.model.Cambio;
+import br.com.william.cambioservice.servico.CambioService;
+import br.com.william.cambioservice.util.EnvironmentUtil;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
-import java.util.Random;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/cambio-service/")
+@RequestMapping("/cambio-service")
+@AllArgsConstructor
 public class CambioEndPoint {
-    private final static Random random = new Random();
-
-    @Autowired
-    private Environment environment;
+    private CambioService cambioService;
+    private EnvironmentUtil environmentUtil;
 
     @GetMapping("/{amount}/{from}/{to}")
-    public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
-                            @PathVariable("from") String from,
-                            @PathVariable("to") String to){
-        return Cambio.builder()
-                .id(random.nextLong())
-                .from(from)
-                .to(to)
-                .conversionFactor(BigDecimal.ONE)
-                .conversionValue(amount)
-                .environment(String.format("PORT %s", getLocalSeverPort()))
-                .build();
+    public ResponseEntity<?> getCambio(@PathVariable("amount") BigDecimal amount,
+                                            @PathVariable("from") String from,
+                                            @PathVariable("to") String to){
+        Cambio cambio = cambioService.findCambioByFromCurrencyAndToCurrency(from, to);
+        cambio = cambioService.CalculateCambioForAmount(cambio, amount);
+        cambio.setEnvironment(environmentUtil.getLocalSeverPort());
+
+        return ResponseEntity.ok(cambio);
     }
 
-    private String getLocalSeverPort(){
-        return environment.getProperty("local.server.port");
+    @GetMapping
+    public ResponseEntity<?> getAll(){
+        List<Cambio> collect = cambioService.getAll()
+                .stream()
+                .map(
+                        e -> {
+                            e.setEnvironment(environmentUtil.getLocalSeverPort());
+                            return e;
+                        }
+                ).collect(Collectors.toList());
+        return ResponseEntity.ok(collect);
     }
+
+
 }
