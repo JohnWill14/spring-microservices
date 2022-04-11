5f86249dcae38a6763ad565b5a044c708ae55a1b
John William <johnwill.v.2017@gmail.com>
2022-04-11 16:22:22 -0300

    trabalhando com open api com microservicos

--- book-service/src/main/java/br/com/william/bookservice/endpoint/BookEndPoint.java
+++ book-service/src/main/java/br/com/william/bookservice/endpoint/BookEndPoint.java
@@ -3,6 +3,8 @@ package br.com.william.bookservice.endpoint;
 import br.com.william.bookservice.model.Book;
 import br.com.william.bookservice.repository.BookRepository;
 import br.com.william.bookservice.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "book endpoint")
 @RestController
 @RequestMapping("/book-service")
 @AllArgsConstructor
 public class BookEndPoint {
     private BookService bookService;
 
+    @Operation(summary = "busca book pelo id")
     @GetMapping("/{id}/{currency}")
     public ResponseEntity<?> getBook(@PathVariable("id") Long id,
                                      @PathVariable("currency") String currency){
--- book-service/src/main/java/br/com/william/bookservice/endpoint/FooBarController.java
+++ book-service/src/main/java/br/com/william/bookservice/endpoint/FooBarController.java
@@ -4,6 +4,8 @@ import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
+@Tag(name = "foo bar endpoint")
 @RestController
 @RequestMapping("/book-service")
 @AllArgsConstructor
 @Log4j2
 public class FooBarController {
+
+    @Operation(summary = "teste com circuit breaker")
     @GetMapping("foo-bar")
 //    @Retry(name = "default", fallbackMethod = "fallbackMethod")
 //    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
