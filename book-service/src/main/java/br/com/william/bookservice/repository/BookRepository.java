8f0324b6eabae6dcb0088a4b506aaa346044ae84
John William <johnwill.v.2017@gmail.com>
2022-04-14 09:30:41 -0300

    move to folder 09-gateway

--- book-service/src/main/java/br/com/william/bookservice/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.william.bookservice.repository;
-
-import br.com.william.bookservice.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {}
